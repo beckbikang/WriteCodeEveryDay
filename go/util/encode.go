6a645d59cc9417bfb3e7199937e8c60d2dc19682
bikang1 <bikang1@staff.weibo.com>
2023-12-12 22:48:51 +0800

    add code

--- go/util/encode.go
+++ go/util/encode.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 )
 
@@ -9,4 +10,9 @@ func GetMd5Str(str string) string {
 	obj := md5.New()
 	obj.Write([]byte(str))
 	return fmt.Sprintf("%x", obj.Sum(nil))
+}
+
+func GetMd5Str2(str string) string {
+	bytes := md5.Sum([]byte(str))
+	return hex.EncodeToString(bytes[:])
 }
--- go/util/encode_test.go
+++ go/util/encode_test.go
@@ -1,9 +1,28 @@
 package util
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 import "github.com/stretchr/testify/assert"
 
 func TestGetMd5Str(t *testing.T) {
 	assertObj := assert.New(t)
 	assertObj.Equal("25d55ad283aa400af464c76d713c07ad", GetMd5Str("12345678"), "12345678 to md5")
+}
+func TestGetMd5Str2(t *testing.T) {
+	assertObj := assert.New(t)
+	assertObj.Equal("25d55ad283aa400af464c76d713c07ad", GetMd5Str2("12345678"), "12345678 to md5")
+}
+
+func BenchmarkGetMd5Str(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		GetMd5Str(strconv.Itoa(i))
+	}
+}
+
+func BenchmarkGetMd5Str2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		GetMd5Str2(strconv.Itoa(i))
+	}
 }
